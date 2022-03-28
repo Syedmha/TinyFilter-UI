2d933a0354c5259bbbe941c7e2ca86245fd79e83
Syedmha <syedabedi12@gmail.com>
2022-03-28 20:36:28 +0530

    input and list components

---
+++ components/input/input.js
@@ -0,0 +1,25 @@
+const emailInput = document.querySelector(".email-inp");
+const errorDiv = document.querySelector(".error");
+
+const regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+
+const enableError = () => {
+    emailInput.style.border = "#a52a2a 2px solid";
+    errorDiv.style.display = "block";
+};
+
+const disableError = () => {
+    emailInput.style.border = "#008000 2px solid";
+    errorDiv.style.display = "none";
+};
+emailInput.addEventListener("input", () => {
+    let value = emailInput.value;
+
+    if (value.length <= 0) {
+        errorDiv.innerHTML = `This field cannot be empty`;
+        enableError();
+    } else if (!regex.test(value)) {
+        errorDiv.innerHTML = `Not a valid email address`;
+        enableError();
+    } else disableError();
+});
